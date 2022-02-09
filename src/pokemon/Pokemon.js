44b6819a449c99f28b397eca33275971f1d69275
edmarsht <edtoulet@gmail.com>
2022-02-09 03:07:52 +0100

    responsive

--- src/pokemon/Pokemon.js
+++ src/pokemon/Pokemon.js
@@ -1,30 +1,34 @@
 import React, { useState, useEffect } from "react";
 import "./Pokemon.css";
 import axios from "axios";
-import { useParams} from "react-router-dom";
+import { useParams } from "react-router-dom";
+import "./Pokemon.css";
 
 function Pokemon() {
+  const params = useParams();
+  console.log(params);
 
-    const params = useParams()
-    console.log(params);
+  const index = params.pokemonIndex;
 
-    const index = params.pokemonIndex;
+  const pokemonImage = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/${index}.gif`;
 
-    const pokemonImage = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/${index}.gif`
+  const [pokemonRes, setPokemonRes] = useState([]);
 
-
-    const [pokemonRes, setPokemonRes] = useState([])
-
-    useEffect(() => {
-      axios.get(`https://pokeapi.co/api/v2/pokemon/${index}`
-      ).then((res) => setPokemonRes(res.data));
-    }, []);
-
+  useEffect(() => {
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon/${index}`)
+      .then((res) => setPokemonRes(res.data));
+  }, []);
 
   return (
-    <div>
-      <h1>Coucou {index}</h1>
-      <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${index}.svg`} alt="" />
+    <div className="pokemon">
+      <div className="pokemon__container">
+        <h1>Coucou</h1>
+        <img
+          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${index}.svg`}
+          alt=""
+        />
+      </div>
     </div>
   );
 }
